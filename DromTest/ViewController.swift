af1977fc0b8c33ca04870f139fc35d0f057824d9
Дягилев Георгий Сергеевич <g.djagilev@cft.ru>
2021-03-09 01:11:36 +0700

    Initial Commit

---
+++ DromTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DromTest
+//
+//  Created by Дягилев Георгий Сергеевич on 09.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
