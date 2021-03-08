f0b54e44499460732bc48687c974887cf9528eec
Дягилев Георгий Сергеевич <g.djagilev@cft.ru>
2021-03-09 01:29:28 +0700

    Implement custom error

--- DromTest/LoadError.swift
+++ DromTest/LoadError.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+enum LoadError: Error
+{
+	case cancelled
+	case noUrl
+	case unKnown
+}
+
+extension LoadError: LocalizedError {
+	var errorDescription: String? {
+		switch self {
+		case .noUrl:
+			return NSLocalizedString("Image URL is incorrect", comment: "")
+		case .cancelled:
+			return NSLocalizedString("Image loading was cancelled", comment: "")
+		case .unKnown:
+			return NSLocalizedString("Unknown error occured", comment: "")
+		}
+	}
+}
