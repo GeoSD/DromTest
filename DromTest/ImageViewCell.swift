5b7c2fbe544ff7e4801b4357c35bb9c3e967fe9e
Дягилев Георгий Сергеевич <g.djagilev@cft.ru>
2021-03-09 01:23:52 +0700

    Implement collectionView cell

---
+++ DromTest/ImageViewCell.swift
@@ -0,0 +1,85 @@
+//
+//  ImageViewCell.swift
+//  DromTest
+//
+//  Created by Дягилев Георгий Сергеевич on 09.03.2021.
+//
+
+import UIKit
+
+protocol IImageViewCell: AnyObject
+{
+	func startLoad()
+	func endLoad(image: UIImage?)
+	var completion: () -> Void { get }
+}
+
+class ImageViewCell: UICollectionViewCell
+{
+	static let cellId = String(describing: self)
+
+	private let imageView = UIImageView()
+	private let activityIndicator = UIActivityIndicatorView()
+
+	var completion: () -> Void = {
+		assertionFailure("Completion must be implemented")
+	}
+
+	override var isSelected: Bool {
+		didSet {
+			UIView.animate(withDuration: 1) { [weak self] in
+				guard let self = self else { return }
+				self.transform = CGAffineTransform(translationX: -self.bounds.width, y: 0)
+				self.completion()
+			}
+		}
+	}
+
+	override init(frame: CGRect) {
+		super.init(frame: frame)
+		self.backgroundColor = .clear
+
+		self.imageView.contentMode = .scaleAspectFill
+		self.imageView.translatesAutoresizingMaskIntoConstraints = false
+		self.contentView.addSubview(self.imageView)
+
+		self.activityIndicator.color = .black
+		self.activityIndicator.hidesWhenStopped = true
+		self.activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+		self.contentView.addSubview(self.activityIndicator)
+
+		NSLayoutConstraint.activate([
+			self.imageView.topAnchor.constraint(equalTo: self.contentView.topAnchor),
+			self.imageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
+			self.imageView.leftAnchor.constraint(equalTo: self.contentView.leftAnchor),
+			self.imageView.rightAnchor.constraint(equalTo: self.contentView.rightAnchor),
+
+			self.activityIndicator.centerXAnchor.constraint(equalTo: self.imageView.centerXAnchor),
+			self.activityIndicator.centerYAnchor.constraint(equalTo: self.imageView.centerYAnchor)
+		])
+		self.contentView.clipsToBounds = true
+	}
+
+	required init?(coder: NSCoder) {
+		fatalError("init(coder:) has not been implemented")
+	}
+}
+
+extension ImageViewCell: IImageViewCell
+{
+	func startLoad() {
+		DispatchQueue.main.async {
+			self.activityIndicator.startAnimating()
+			self.activityIndicator.isHidden = false
+			self.imageView.image = nil
+		}
+	}
+
+	func endLoad(image: UIImage?) {
+		DispatchQueue.main.async {
+			self.activityIndicator.stopAnimating()
+			self.activityIndicator.isHidden = true
+			self.imageView.image = image
+		}
+	}
+}
